5e0b74e88c66b956bc8dcc569f427628482eabed
Shareef <shareefnncr@gmail.com>
2026-01-27 22:24:33 +0530

    Remove redundant sections from Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,9 @@ import { Navbar } from "@/components/layout/Navbar";
 import { Footer } from "@/components/layout/Footer";
 import { Hero } from "@/components/sections/Hero";
 import { AboutSummary } from "@/components/sections/AboutSummary"; // New
-import { FeaturedTopics } from "@/components/sections/FeaturedTopics";
 import { Team } from "@/components/sections/Team"; // New
 import { Courses } from "@/components/sections/Courses";
 import { Process } from "@/components/sections/Process"; // New
-import { Benefits } from "@/components/sections/Benefits";
 import { Community } from "@/components/sections/Community"; // Restored
 import { Testimonials } from "@/components/sections/Testimonials"; // New
 import { FAQ } from "@/components/sections/FAQ";
@@ -34,11 +32,8 @@ export default function Home() {
         
         {/* 2. About Academy (Who We Are) */}
         <AboutSummary />
-        
-        {/* 3. Featured Topics (Value) */}
-        <FeaturedTopics />
 
-        {/* 3.5. Team Section (Credibility) */}
+        {/* 3. Team Section (Credibility) */}
         <Team />
         
         {/* 4. Courses Section (Core Business) */}
@@ -47,19 +42,16 @@ export default function Home() {
         {/* 5. How Learning Works (Process) */}
         <Process />
         
-        {/* 6. Benefits (Why Choose Us) */}
-        <Benefits />
-        
-        {/* 7. Exclusive Community (Trust + Connection) */}
+        {/* 6. Exclusive Community (Trust + Connection) */}
         <Community />
         
-        {/* 8. Testimonials (Social Proof) */}
+        {/* 7. Testimonials (Social Proof) */}
         <Testimonials />
         
-        {/* 9. FAQ (Support) */}
+        {/* 8. FAQ (Support) */}
         <FAQ />
         
-        {/* 10. Call To Action (Final Push) */}
+        {/* 9. Call To Action (Final Push) */}
         <CTA />
         
         {/* 11. Footer (Closure) */}
